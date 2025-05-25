43b4723bce1befeea732ad44620c9337d986cc38
Shahwatrajsingh <shashwatrajsing@gmail.com>
2025-05-25 14:49:01 +0530

    first commit

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@nestjs/common";
+import { user, Bookmark } from "generated/prisma";
+
+@Injectable({})
+export class AuthService{
+    
+
+signup(){
+    return `you're signed up`
+}
+
+signin(){
+    return `you are signed in`
+}
+}
